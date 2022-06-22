99a1739b8761b669eeaf72df4cb901a31f887b52
Dyunace <tmddn5924@gmail.com>
2022-06-23 01:56:32 +0900

    // Framework v0.4 h

--- Framework/Framework/Main.cpp
+++ Framework/Framework/Main.cpp
@@ -1,75 +1,174 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "Parent.h"
 #include "Child.h"
 #include "Bullet.h"
 
-
-// ** 1. 정보은닉
-//		private:	= 자기 자신만 사용 가능.
-//		protected:	= 상속간의 공개된 상태. 비상속 클레스 사용불가. 외부 사용 불가.
-//		public:		= 공개된 상태
-
-
-// ** 2. 캡슐화
-//		데이터(변수)와 기능(함수)을 묶음으로 사용하는 것.
-
-
-// ** 3. 추상화
-//		존재하지 않는 형태를 추상 클래스 라고 한다.
-
-//		예:) 사람, 학생, 군인 등의 대상이 특정되지 않는 경우.(추상적인 경우.)
-//			 "홍길동은 학생이다." 라면 홍길동이라는 특정 대상이 있으므로,
-//			 추상적 형태로 볼 수 없음.
-
-
-// ** 4. 상속
+// ** 7. 생성자 & 소멸자 & 복사 생성자.
+// ** 8. 깊은복사 얕은복사.
+// ** (복사 생성자)
 /*
-	class AAA
+struct tagInfo
+{
+	int Number;
+};
+
+class Object
+{
+protected:
+	tagInfo m_Info;
+public:
+	int Number;
+	char* Name;
+public:
+	virtual Object* Clone() = 0;
+
+
+
+
+public:
+	// ** 사용자가 호출하지 않아도 스스로 호출됨.
+	// ** 클래스가 생성된 직후 호출.
+	Object()
 	{
-	protected:
-		string m_str;
-	public:
-		void Output() { cout << m_str << endl; }
+		cout << "생성자" << endl;
 	};
 
-	class BBB : public AAA
+	// ** 사용자가 호출하는 시점에 호출 됨.
+	// ** 전달값의 개수와 자료형에 따라서 선택적(자동)으로 호출 됨.
+	Object(int _Number) : Number(_Number)
 	{
-	public:
-		void Initialize() { m_str = "BBB"; }
+		cout << "복사 생성자 : int" << endl;
+
+		//Number = _Number;
 	};
 
-	class CCC : public AAA
+	Object(float _Number)
 	{
-	public:
-		void Initialize() { m_str = "CCC"; }
+		cout << "복사 생성자 : float" << endl;
+
+		Number = (int)_Number;
 	};
 
+	Object(char* _Name)
+	{
+		Name = new char[strlen(_Name) + 1];
+		strcpy(Name, _Name);
+	};
 
-	[main 출력]
-	//BBB b;
-	//b.Initialize();
-	//b.Output();
-	//
-	//CCC c;
-	//c.Initialize();
-	//c.Output();
+	Object(Object* _Obj)
+	{
+		Name = new char[strlen(_Obj->Name) + 1];
+		strcpy(Name, _Obj->Name);
+	};
+
+	Object(tagInfo _Info) : m_Info(_Info) { }
+
+
+
+	// ** 클래스가 삭제되기 직전
+	~Object()
+	{
+		cout << "소멸자" << endl;
+
+		//delete[] Name;
+		//Name = nullptr;
+	};
+};
+
+class Player : public Object
+{
+public:
+	virtual Object* Clone() override { return new Player(*this); }
+
+	void Output() { cout << m_Info.Number << endl; }
+
+public:
+	Player() { };
+	//Player(tagInfo _Info) : Object(_Info) { };
+	Player(tagInfo _Info) : Object(_Info) { };
+	~Player() { };
+};
 */
 
-// ** 5. 다형성
-//		다양한 형태로 존재할 수 있다.
 
-// ** 6. namespace
-//		별도의 영역을 지정하여 동일한 이름의 함수를 사용할 수 있도록 함.
-
-
-// ** 7. 생성자 & 소멸자 & 복사 생성자.
-
-// ** 8. 깊은복사 얕은복사.
 
 // ** 9. 오버로딩 & 오버라이딩.
+/*
+class Object
+{
+public:
+	virtual void Output()
+	{
+		cout << "Object : " << endl;
+	}
+
+	virtual void Output(string _str)
+	{
+		cout << "Object : " << _str << endl;
+	}
+};
+
+class AAA : public Object
+{
+public:
+	void Output() override
+	{
+		cout << "AAA : " << endl;
+	}
+
+	void Output(string _str) override
+	{
+		cout << "AAA : " << _str << endl;
+	}
+};
+*/
+
+
+
 
 // ** 10. 연산자 오버로딩.
 
-// ** 11. 포인터 (복습).
+struct tagInfo
+{
+	int Number;
+
+	tagInfo() {};
+	tagInfo(int _Number) : Number(_Number) {};
+};
+
+class Object
+{
+private:
+	tagInfo Info;
+public:
+	Object& operator+=(const Object& _Obj)
+	{
+		Info.Number += _Obj.Info.Number;
+		return *this;
+	}
+
+	Object& operator++()
+	{
+		Info.Number += 1;
+		return *this;
+	}
+
+	void Output()
+	{
+		cout << Info.Number << endl;
+	}
+public:
+	Object() { }
+	Object(tagInfo _Info) : Info(_Info) { }
+	~Object() { }
+};
+
+
+
+
+
+
+// ** 11. 포인터 (복습) & 캡슐화.
 
 
 
@@ -78,33 +177,94 @@
 
 
 
-const int ID_Child = 0;
-const int ID_Bullet = 1;
 
+
+
+//map<string, Object*> PortotypeObject;
+
+//Object* GetObject(string _Key);
 
 int main(void)
 {
-	Parent* p[2];
+	/*
+	Object o = Object(3.141592f);
+	cout << "Hello World!!" << endl;
 
-	p[ID_Child] = new Child;
-	p[ID_Bullet] = new Bullet;
+	Object o1((char*)"홍길동");
+	//Object o2 = o1;
+	Object o2(o1);
+	*/
 
-	for (int i = 0; i < 2; ++i)
-		p[i]->Initialize();
 
-	while (true)
+	/*
+	int i = 10;
+	int n(10);
+	*/
+
+
+
+	/*
+	tagInfo Info;
+	AAA a[8];
+
+	for (int i = 0; i < 8; ++i)
 	{
-		system("cls");
-		for (int i = 0; i < 2; ++i)
-			p[i]->Update();
-
-		for (int i = 0; i < 2; ++i)
-			p[i]->Output();
+		Info.Number = i + 1;
+		a[i] = AAA(Info);
 	}
 
+	for (int i = 0; i < 8; ++i)
+		a[i].Output();
+	*/
 
 
+
+	/*
+	 * 예제
+	tagInfo Info;
+
+	Info.Number = 10;
+	PortotypeObject["Player"] = new Player(Info);
+
+	Object* pProtoObj = GetObject("Player");
+
+	Object* pPlayer = nullptr;
+
+	if (pProtoObj != nullptr)
+		pPlayer = pProtoObj->Clone();
+	*/
+
+
+
+
+
+	/*
+	Object o1(tagInfo(10));
+
+	Object o2(o1);
+
+	o2 += o1;
+
+	++o2;
+
+	o2.Output();
+	*/
 
 	return 0;
 }
+
+
+
+
+/*
+Object* GetObject(string _Key)
+{
+	map<string, Object*>::iterator iter = PortotypeObject.find(_Key);
+
+	if (iter == PortotypeObject.end())
+		return nullptr;
+	else
+		return iter->second;
+}
+*/
 
